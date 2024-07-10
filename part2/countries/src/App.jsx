c10eb11a18f4927602c6f2596a58dc48926d5d56
naomeii <1naomiari@gmail.com>
2024-07-09 21:39:12 -0400

    finished countries exercise for part 2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,17 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const ShowCountry = ({currCountryObj}) => {
+const ShowCountry = ({currCountryObj, weatherObj}) => {
+  if (!weatherObj) {
+    // hasn't finished rendering
+    return null;
+  }
+  
+  const temp = ((weatherObj.main.temp) - 273.15).toFixed(2);
+  const icon = weatherObj.weather[0].icon;
+  const desc = weatherObj.weather[0].description;
+  const windSpeed = weatherObj.wind.speed;
+
   return (
     <>
       <h1> {currCountryObj.name.common} </h1>
@@ -22,16 +32,26 @@ const ShowCountry = ({currCountryObj}) => {
       ))}
       </ul>
       <img src={currCountryObj.flags.png} alt={currCountryObj.flags.alt} width='150'/>
+      <h1>Weather in {currCountryObj.capital}</h1>
+      <p>
+        temperature {`${temp} celsius`}
+      </p>
+      <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt={desc} />
+      <p>
+        wind {`${windSpeed} m/s`}
+      </p>
     </>
   )
 }
 
+
 function App() {
+  const api_key = import.meta.env.VITE_SOME_KEY
   const [searchValue, setSearchValue] = useState('');
   const [countries, setCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [countrySelected, setCountrySelected] = useState(null);
-
+  const [weatherObj, setWeatherObj] = useState(null);
 
   const handleChange = (event) => {
     setSearchValue(event.target.value)
@@ -43,12 +63,12 @@ function App() {
   }
 
   const displayResults = () => {
-    if (filteredCountries){
+    if (filteredCountries || countrySelected){
       if (countrySelected) {
-        return <ShowCountry currCountryObj={countrySelected} />;
+        return <ShowCountry currCountryObj={countrySelected} weatherObj={weatherObj}/>;
       }
       if (filteredCountries.length === 1){
-        return <ShowCountry currCountryObj={filteredCountries[0]}/>
+        return <ShowCountry currCountryObj={filteredCountries[0]} weatherObj={weatherObj}/>
       } 
       else if (filteredCountries.length > 10){
         return <>Too many matches, specify another filter</>
@@ -74,12 +94,22 @@ function App() {
   // gets all country objects and puts in countries
   useEffect(() => {
     axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
-    .then(returnedCountries => 
-      {
-        setCountries(returnedCountries.data);
-      })
+    .then(returnedCountries => setCountries(returnedCountries.data))
   }, [])
-  
+
+  // fetch weather data
+  useEffect(() =>
+    {
+      if (filteredCountries.length == 1 || countrySelected) {
+        const currCountryObj = filteredCountries[0] || countrySelected
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${currCountryObj.capital},${currCountryObj.cca2}&appid=${api_key}`)
+        .then(returnedWeather => setWeatherObj(returnedWeather.data))
+        .catch(error => 
+          console.log(`${error} No capital was found`)
+        )
+      }
+    }, [filteredCountries]
+  )  
 
   return (
     <div>
@@ -87,7 +117,6 @@ function App() {
         find countries  <input type="text" value={searchValue} onChange={handleChange} />
       </form>
       {displayResults()}
-
     </div>
   )
 }
