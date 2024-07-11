7820b9548b1d138b91b1d42b3ddbfb4a38943322
naomeii <1naomiari@gmail.com>
2024-07-10 22:34:48 -0400

    all lecture code up to date

---
+++ lectures/part1/part1/src/App.jsx
@@ -0,0 +1,195 @@
+import { useState } from 'react'
+
+
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  )
+}
+
+
+
+// ----------------------------
+// const History = (props) => {
+
+//   if (props.allClicks.length === 0) {
+//     return (
+//       <div>
+//         the app is used by pressing the buttons
+//       </div>
+//     )
+//   }
+
+//   return (
+//     <div>
+//       button press history: {props.allClicks.join(' ')}
+//     </div>
+//   )
+// }
+
+
+// const Button = (props) => { 
+
+//   // console.log('props value is', props);
+//   const { handleClick, text } = props
+//   return (
+//     <button onClick={handleClick}>
+//       {text}
+//     </button>
+//   )
+// }
+
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     setLeft(left + 1)
+//   }
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     setRight(right + 1)
+//   }
+
+//   // debugger
+
+//   return (
+//     <div>
+//       {left}
+
+//       <Button handleClick={handleLeftClick} text='left' />
+//       <Button handleClick={handleRightClick} text='right' />
+//       {right}
+//       <History allClicks={allClicks} />
+//     </div>
+//   )
+// }
+
+
+/// -----------------------------------Part 1C
+// const Display = ({ counter }) => <div>{counter}</div>
+
+// const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   console.log('rendering with counter value', counter)
+
+//   const increaseByOne = () => {
+
+//     console.log('increasing, value before', counter)
+//     setCounter(counter + 1)
+//   }
+
+//   const decreaseByOne = () => { 
+
+//     console.log('decreasing, value before', counter)
+//     setCounter(counter - 1)
+//   }
+
+//   const setToZero = () => {
+
+//     console.log('resetting to zero, value before', counter)
+//     setCounter(0)
+//   }
+
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button onClick={increaseByOne} text="plus" />
+//       <Button onClick={setToZero} text="zero" />
+//       <Button onClick={decreaseByOne} text="minus" />
+//     </div>
+//   )
+// } 
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
