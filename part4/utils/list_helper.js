b229767b0e56ef325aefa438a7787cf49eeb8ddc
naomeii <1naomiari@gmail.com>
2024-07-13 15:57:34 -0400

    4.5, gonna do 4.6-4.7 later

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,27 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null;
+    }
+    
+    const topBlog = blogs.reduce((maxBlog, currentBlog) => {
+        return currentBlog.likes > maxBlog.likes ? currentBlog : maxBlog;
+    }, blogs[0]); // Initialize maxBlog with the first blog
+    
+    return {
+        title: topBlog.title,
+        author: topBlog.author,
+        likes: topBlog.likes
+    };
+}
+  
+module.exports = {
+    dummy, totalLikes, favoriteBlog
+}
