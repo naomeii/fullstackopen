b89fc758593a2f6727648d878dfd2f9bf9b1ba73
naomeii <1naomiari@gmail.com>
2024-07-13 15:10:15 -0400

    4.2 completed

---
+++ part4/models/blog.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString() // converst _id to id
+      delete returnedObject._id
+      delete returnedObject.__v // version key is used by Mongoose not rly needed for us
+    }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
